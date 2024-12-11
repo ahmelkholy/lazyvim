2b7313c1d27c799062a6c56454fb3a709ee34a5b
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-11 16:33:33 +0300

    to go

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -84,7 +84,7 @@ return {
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -139,20 +139,19 @@ return {
   },
 
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+  -- If you'd rather extend the default config, use the code below instead:
+  -- would overwrite `ensure_installed` with the new value.
+  -- ... previous plugin configurations ...
+
+  -- Since `vim.tbl_deep_extend` can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-      })
-    end,
-  },
+    enabled = false,
+  }, -- Add a comma here
 
-  -- the opts function can also be used to change the default opts:
+  -- The opts function can also be used to change the default opts:
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
@@ -163,26 +162,26 @@ return {
         end,
       })
     end,
-  },
+  }, -- Add a comma here
 
-  -- or you can return new options to override all the defaults
+  -- Or you can return new options to override all the defaults
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function()
       return {
-        --[[add your custom lualine config here]]
+        -- Add your custom lualine config here
       }
     end,
-  },
+  }, -- Add a comma here
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  -- Use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" }, -- Add a comma here
 
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  -- Add jsonls and schemastore packages, and setup treesitter for json, json5, and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" }, -- Add a comma here
 
-  -- add any tools you want to have installed below
+  -- Add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
     opts = {
