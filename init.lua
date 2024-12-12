ffc9633f49539caf57feaec435f62473182f90ad
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-13 01:56:25 +0300

    "regular automatic update"

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require('nvim-treesitter.install').compilers = { "clang" }
+-- Remove or comment out the following line if present:
+-- neovim.stop()
