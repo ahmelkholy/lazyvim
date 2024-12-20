aee30df32e79f86653ce451f42b16c81522f223a
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-21 00:34:33 +0300

    "working probably"

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
---require('nvim-treesitter.install').compilers = { "clang" }
-require("nvim-treesitter.install").compilers = { "gcc" }
+require("nvim-treesitter.install").compilers = { "clang" }
+--require("nvim-treesitter.install").compilers = { "gcc" }
+require("nvim-treesitter.install").prefer_git = false
 -- Remove or comment out the following line if present:
 -- neovim.stop()
